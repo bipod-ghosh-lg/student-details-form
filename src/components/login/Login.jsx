6c2cff372f5e1962c9d59deace754383414ac05b
Bipod Ghosh <bipodghosh.lg@gmail.com>
2024-07-01 19:45:54 +0530

    done for both screen

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -25,13 +25,13 @@ const Login = () => {
   };
 
   return (
-    <div className="h-full bg-gray-100 min-h-screen w-screen md:px-12 xl:px-32 2xl:px-40 py-4 md:py-12 2xl:py-10 flex flex-col md:flex-row justify-between overflow-hidden">
-      <div className="absolute top-0 right-0 h-20 w-72 rounded-bl-lg bg-[#00BDD6]" />
+    <div className="h-full bg-gray-100 min-h-screen w-screen px-5 md:px-12 xl:px-32 2xl:px-40 py-4 md:py-12 2xl:py-10 gap-10 md:gap-0 flex flex-col-reverse md:flex-row justify-between overflow-hidden">
+      <div className="absolute top-0 right-0 h-20 w-32 md:w-72 rounded-bl-lg bg-[#00BDD6]" />
       <div className="absolute bottom-0 left-0 h-48 2xl:h-60 w-96 rounded-tr-lg bg-[#00BDD6] " />
       <div className="absolute bottom-[18%] left-[33%] 2xl:left-[25%] h-[20vh] w-[22%]">
         <img src={LoginDotElement} alt="" className="bg-inherit" />
       </div>
-      <div className=" absolute top-7 left-10 z-30 flex items-center justify-start gap-4">
+      <div className=" absolute top-3 md:top-7 left-7 md:left-10 z-30 flex items-center justify-start gap-4">
         <div className="w-16 h-16 flex items-center ">
           <img src={logo} alt="" />
         </div>
@@ -41,7 +41,7 @@ const Login = () => {
       </div>
 
       <div className="h-auto w-full flex flex-col justify-center items-start gap-4  ">
-        <div className=" h-full bg-white max-h-[70%] w-[80%] z-20 md:w-[70%] xl:w-[60%] 2xl:w-[50%] relative  flex flex-col justify-center items-center rounded-lg shadow-lg  gap-4 xl:gap-5  px-4 py-5 xl:py-5 ">
+        <div className=" h-full bg-white max-h-[70%] w-full  z-20 md:w-[70%] xl:w-[60%] 2xl:w-[50%] relative  flex flex-col justify-center items-center rounded-lg shadow-lg  gap-4 xl:gap-5  px-4 py-5 xl:py-5 ">
           <div className="w-full flex flex-col justify-around items-center gap-4">
             <div className="flex gap-2 items-center">
               <h1 className=" text-3xl font-bold">Welcome Back</h1>
@@ -102,9 +102,9 @@ const Login = () => {
           </div>
         </div>
       </div>
-      <div className="h-full w-[50%] flex flex-col gap-5 justify-center items-end  z-10   ">
-        <div className=" w-[100%]   ">
-          <img src={loginHeroImg} alt="" className=" object-contain" />
+      <div className="h-full w-full md:w-[50%] flex flex-col gap-5 justify-center items-end mt-12 md:mt-0  z-10   ">
+        <div className=" w-[100%] flex justify-center">
+          <img src={loginHeroImg} alt="" className=" object-contain h-[70%] md:h-full w-[70%] md:w-full"  />
         </div>
         <div className="w-full flex flex-col  gap-10 justify-between items-center text-[#00BDD6]">
           <div className="w-full flex justify-between items-center relative">
@@ -113,7 +113,7 @@ const Login = () => {
             <hr className="w-[70%] bg-[#00BDD6] h-[2px]" />
           </div>
           <div className=" w-full text-[#00BDD6]">
-            <p className="text-3xl font-semibold text-center">
+            <p className="text-2xl lg:text-3xl font-semibold text-center">
               Join us to <span className=" underline">effectively</span>{" "}
               organize your schedule, manage events and stay on top of you busy
               life
