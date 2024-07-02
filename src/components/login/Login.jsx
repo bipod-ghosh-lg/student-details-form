5da376a2a7a56d8fe8ce08faa4dac179dcd7a3e2
Bipod Ghosh <bipodghosh.lg@gmail.com>
2024-07-02 12:02:54 +0530

    done

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -11,10 +11,12 @@ import smileImg from "../../assets/images/smile.png";
 import { AiOutlineMail } from "react-icons/ai";
 import { IoLockClosedOutline } from "react-icons/io5";
 import { IoEye } from "react-icons/io5";
+import { AiFillEyeInvisible } from "react-icons/ai";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -24,11 +26,15 @@ const Login = () => {
     dispatch(setUser({ email, password }));
   };
 
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div className="h-full bg-gray-100 min-h-screen w-screen px-5 md:px-12 xl:px-32 2xl:px-40 py-4 md:py-12 2xl:py-10 gap-10 md:gap-0 flex flex-col-reverse md:flex-row justify-between overflow-hidden">
-      <div className="absolute top-0 right-0 h-20 w-32 md:w-72 rounded-bl-lg bg-[#00BDD6]" />
-      <div className="absolute bottom-0 left-0 h-48 2xl:h-60 w-96 rounded-tr-lg bg-[#00BDD6] " />
-      <div className="absolute bottom-[18%] left-[33%] 2xl:left-[25%] h-[20vh] w-[22%]">
+    <div className="h-full bg-gray-100 min-h-screen w-screen px-5 md:px-12 xl:px-32 2xl:px-72 py-4 md:py-12 2xl:py-10 gap-10 md:gap-0 flex flex-col-reverse md:flex-row justify-between items-center overflow-hidden">
+      <div className="absolute top-0 right-0 h-20 md:h-[15vh] 2xl:h-[17vh] w-28  md:w-72 2xl:w-[25vw] rounded-bl-lg bg-[#00BDD6]" />
+      <div className="absolute bottom-0 left-0 h-48 2xl:h-60 w-72 md:w-96 2xl:w-[30vw] rounded-tr-lg bg-[#00BDD6] " />
+      <div className="absolute bottom-[18%] left-[33%] 2xl:left-[33%] h-[20vh] w-[22%]">
         <img src={LoginDotElement} alt="" className="bg-inherit" />
       </div>
       <div className=" absolute top-3 md:top-7 left-7 md:left-10 z-30 flex items-center justify-start gap-4">
@@ -41,11 +47,11 @@ const Login = () => {
       </div>
 
       <div className="h-auto w-full flex flex-col justify-center items-start gap-4  ">
-        <div className=" h-full bg-white max-h-[70%] w-full  z-20 md:w-[70%] xl:w-[60%] 2xl:w-[50%] relative  flex flex-col justify-center items-center rounded-lg shadow-lg  gap-4 xl:gap-5  px-4 py-5 xl:py-5 ">
+        <div className=" h-a bg-white  w-full  z-20 md:w-[70%] xl:w-[60%] 2xl:w-[70%] relative  flex flex-col justify-center items-center rounded-lg shadow-lg border  gap-8 md:gap-4 xl:gap-5 2xl:gap-7  px-4 2xl:px-12 py-5 xl:py-10 2xl:py-20  ">
           <div className="w-full flex flex-col justify-around items-center gap-4">
-            <div className="flex gap-2 items-center">
+            <div className="flex gap-2 items-center py-5 md:py-0">
               <h1 className=" text-3xl font-bold">Welcome Back</h1>
-              <img src={smileImg} alt="" className=" h-12 w-12" />
+              <img src={smileImg} alt="" className=" h-8 w-8 md:h-12 md:w-12" />
             </div>
             <h2 className=" text-xl text-gray-500">Sign in your account</h2>
           </div>
@@ -65,14 +71,24 @@ const Login = () => {
               <div className="flex gap-4 items-center">
                 <IoLockClosedOutline className="text-gray-700 text-xl" />
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   placeholder="Enter your Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   className="w-full bg-inherit outline-none"
                 />
               </div>
-              <IoEye className="text-gray-700 text-xl" />
+              {showPassword ? (
+                <AiFillEyeInvisible
+                  className="text-gray-700 text-xl"
+                  onClick={handleShowPassword}
+                />
+              ) : (
+                <IoEye
+                  className="text-gray-700 text-xl"
+                  onClick={handleShowPassword}
+                />
+              )}
             </div>
 
             <button
@@ -93,27 +109,31 @@ const Login = () => {
             </p>
           </div>
 
-          <div className="flex flex-col items-center justify-center w-full">
+          <div className="flex flex-col items-center justify-center w-full pb-7 md:pb-0">
             <p className="">Have Doubts?</p>
-            <div className="flex justify-around gap-2  w-10 h-5">
+            <div className="flex justify-around gap-2  w-10 h-5 ">
               <img src={whatsappImg} alt="" className="w-5" />
               <img src={questionMarkImg} alt="" className="w-5" />
             </div>
           </div>
         </div>
       </div>
-      <div className="h-full w-full md:w-[50%] flex flex-col gap-5 justify-center items-end mt-12 md:mt-0  z-10   ">
+      <div className="h-full w-full md:w-[60%]  flex flex-col gap-5 justify-center items-end mt-12 md:mt-0  z-10   ">
         <div className=" w-[100%] flex justify-center">
-          <img src={loginHeroImg} alt="" className=" object-contain h-[70%] md:h-full w-[70%] md:w-full"  />
+          <img
+            src={loginHeroImg}
+            alt=""
+            className=" object-contain h-full md:h-full w-full md:w-full"
+          />
         </div>
-        <div className="w-full flex flex-col  gap-10 justify-between items-center text-[#00BDD6]">
+        <div className="w-full flex flex-col  gap-14 justify-between items-center text-[#00BDD6]">
           <div className="w-full flex justify-between items-center relative">
             <hr className="w-[20%] bg-[#00BDD6] h-[2px]" />
             <span className="text-5xl font-bold h-[2px]">“</span>
             <hr className="w-[70%] bg-[#00BDD6] h-[2px]" />
           </div>
           <div className=" w-full text-[#00BDD6]">
-            <p className="text-2xl lg:text-3xl font-semibold text-center">
+            <p className=" text-xl md:text-2xl lg:text-3xl font-semibold text-center">
               Join us to <span className=" underline">effectively</span>{" "}
               organize your schedule, manage events and stay on top of you busy
               life
